6ea69c85525e2693a681d941e8a05503cc1585a3
Marek Ograbek <36967486+mareko77@users.noreply.github.com>
2024-11-24 21:43:24 +0000

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User'); // Adjust to your User model's location
 
 const handleRegister = async (req, res) => {
