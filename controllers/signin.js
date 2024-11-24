5bfe1f34876562b25ec01fcd2b364cd5894437a1
Marek Ograbek <36967486+mareko77@users.noreply.github.com>
2024-11-24 21:43:40 +0000

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User'); // Adjust the path as necessary
 const handleSignin = async (req, res, mongoose, bcrypt) => {
   const { email, password } = req.body;
