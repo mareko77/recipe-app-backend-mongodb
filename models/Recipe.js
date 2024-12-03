392adf084f6f52d185489d8e713f202aa0090723
Marek Ograbek <36967486+mareko77@users.noreply.github.com>
2024-12-03 12:08:43 +0000

    Update Recipe.js

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,7 +4,8 @@ const RecipeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
-    ingredients: { type: [String], required: true }, // Array of strings
+    ingredients: { type: [String], required: true },
+    instructions: { type: String, required: true },
     url_photo: { type: String },
 });
 
